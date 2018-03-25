de98b90b57834d6d51cf16bae6b348594a842494
Muhamad Reza Pahlevi <36880832+Necropay@users.noreply.github.com>
2018-03-25 22:40:58 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<meta charset="utf-8">
+<head>
+	<title>Web Jualan</title>
+	<style type="text/css">
+		body{
+			background-color: black;
+			color: white;
+			width: auto;
+			height: 900px;
+		}
+		#container{
+			width: auto;
+			height: 500px;
+			background-color: white;
+			margin: 0;
+		}
+		
+			 
+		#menu{
+			width: auto;
+			height: 100px;
+			margin: 0px;
+			background-color: red;
+		}
+		li {
+			float: left;
+			margin-right: 100px;
+		}
+	</style>	
+</head>
+<body>
+	<div id="container">
+		<div id="menu">
+			<ul class="bar">
+				<li id="home"><a href="">Home</a></li>
+				<li id="gallery"><a href="">Gallery</a></li>
+				<li id="game"><a href="">Game List</a></li>
+				<li id="sign"><a href="">Sign Up</a></li>
+			</ul>
+		</div>
+	</div>
+</body>
+</html>
